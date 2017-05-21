60a15ba6fc2083815c2c114780f18a39fab27d42
prakash2blr <prakash2blr@gmail.com>
2017-05-21 23:35:50 +0530

    added all files

---
+++ main.js
@@ -0,0 +1,657 @@
+function imgToCanvas1(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_1[j+","+i+",1,1"]){
+				cx.fillStyle=array_1[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas2(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_2[j+","+i+",1,1"]){
+				cx.fillStyle=array_2[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas3(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_3[j+","+i+",1,1"]){
+				cx.fillStyle=array_3[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas4(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_4[j+","+i+",1,1"]){
+				cx.fillStyle=array_4[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas5(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_5[j+","+i+",1,1"]){
+				cx.fillStyle=array_5[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas6(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_6[j+","+i+",1,1"]){
+				cx.fillStyle=array_6[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas7(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_7[j+","+i+",1,1"]){
+				cx.fillStyle=array_7[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas8(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_8[j+","+i+",1,1"]){
+				cx.fillStyle=array_8[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas9(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_9[j+","+i+",1,1"]){
+				cx.fillStyle=array_9[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas10(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_10[j+","+i+",1,1"]){
+				cx.fillStyle=array_10[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas11(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_11[j+","+i+",1,1"]){
+				cx.fillStyle=array_11[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas12(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_12[j+","+i+",1,1"]){
+				cx.fillStyle=array_12[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas13(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_13[j+","+i+",1,1"]){
+				cx.fillStyle=array_13[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas14(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_14[j+","+i+",1,1"]){
+				cx.fillStyle=array_14[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas15(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_15[j+","+i+",1,1"]){
+				cx.fillStyle=array_15[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas16(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_16[j+","+i+",1,1"]){
+				cx.fillStyle=array_16[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas17(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_17[j+","+i+",1,1"]){
+				cx.fillStyle=array_17[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas18(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_18[j+","+i+",1,1"]){
+				cx.fillStyle=array_18[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas19(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_19[j+","+i+",1,1"]){
+				cx.fillStyle=array_19[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas20(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_20[j+","+i+",1,1"]){
+				cx.fillStyle=array_20[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas21(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_21[j+","+i+",1,1"]){
+				cx.fillStyle=array_21[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas22(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_22[j+","+i+",1,1"]){
+				cx.fillStyle=array_22[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas23(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_23[j+","+i+",1,1"]){
+				cx.fillStyle=array_23[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas24(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_24[j+","+i+",1,1"]){
+				cx.fillStyle=array_24[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas25(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_25[j+","+i+",1,1"]){
+				cx.fillStyle=array_25[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas26(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_26[j+","+i+",1,1"]){
+				cx.fillStyle=array_26[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas27(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_27[j+","+i+",1,1"]){
+				cx.fillStyle=array_27[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas28(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_28[j+","+i+",1,1"]){
+				cx.fillStyle=array_28[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas29(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_29[j+","+i+",1,1"]){
+				cx.fillStyle=array_29[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas30(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_29[j+","+i+",1,1"]){
+				cx.fillStyle=array_29[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas31(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_30[j+","+i+",1,1"]){
+				cx.fillStyle=array_30[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas32(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_30[j+","+i+",1,1"]){
+				cx.fillStyle=array_30[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas33(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_30[j+","+i+",1,1"]){
+				cx.fillStyle=array_30[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas34(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_30[j+","+i+",1,1"]){
+				cx.fillStyle=array_30[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas35(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_30[j+","+i+",1,1"]){
+				cx.fillStyle=array_30[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas36(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_30[j+","+i+",1,1"]){
+				cx.fillStyle=array_30[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas37(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_30[j+","+i+",1,1"]){
+				cx.fillStyle=array_30[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas38(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_30[j+","+i+",1,1"]){
+				cx.fillStyle=array_30[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas39(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_30[j+","+i+",1,1"]){
+				cx.fillStyle=array_30[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas40(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_30[j+","+i+",1,1"]){
+				cx.fillStyle=array_30[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+function imgToCanvas41(cx){
+	//cx.fillStyle="rgba(10,35,39,1)";
+		
+	for(var i=0;i<=199;i++){
+		for(var j=0;j<=199;j++){
+			if(array_30[j+","+i+",1,1"]){
+				cx.fillStyle=array_30[j+","+i+",1,1"];
+			        cx.fillRect(j,i,1,1);
+			}	
+			else{
+			        cx.fillRect(j,i,1,1);
+			}
+		}
+	}
+	
+}
+
